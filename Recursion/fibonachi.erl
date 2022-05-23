32275adab660b5a47613133d634eca26bf021ed1
Zefir <stickulzon@gmail.com>
2022-05-23 19:20:44 +0300

    fibonachi benchmark add

--- Recursion/fibonachi.erl
+++ Recursion/fibonachi.erl
@@ -1,9 +1,21 @@
 -module(fibonachi).
 
--export([fib/1]).
+-export([fib/1, start_test1/2]).
 
 fib(N) when N=<2 -> N;
 fib(N) -> fib(N-2, 1, 1).
 
 fib(0, _, Acc2) -> Acc2;
 fib(N1, Acc1, Acc2) -> fib(N1 - 1, Acc2, Acc1+Acc2).
+
+
+start_test1(N, CallsNum) -> 
+    T1 = erlang:system_time(millisecond), 
+    test1(N, CallsNum),
+    T2 = erlang:system_time(millisecond),
+    io:format("~p milliseconds~n",[T2 - T1]).
+
+test1(_, 0) -> ok;
+test1(N, CallsNum) -> 
+    fib(N),
+    test1(N, CallsNum-1).
